900d46588205b107d980e36101ccc68faa013cc3
Dhruv Thakkar <106736371+Dhruv100404@users.noreply.github.com>
2023-10-19 08:33:07 +0530

    SwapNShop v.1

---
+++ backend/models/product.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+// import User from "./user";
+
+const transactionSchema = new mongoose.Schema({
+    productName: String,
+    productType: String,
+    issuedPrice: Number,
+    //finalPrice: Number,
+    issuedDate: Date,
+    image : String,
+    productDescription: String,
+    offers: {
+        type: [
+            {
+                user: mongoose.Schema.Types.ObjectId,
+                offerprice: Number,
+                username: String,
+                useremail:String,
+                userphone : String,
+            },
+        ],
+    },
+    sold: {
+        type: Boolean,
+        default: false,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    //sellDate: Date,
+});
+const Product = mongoose.model("Product", transactionSchema);
+// userSchema.plugin(passportLocalMongoose);
+export default Product;
